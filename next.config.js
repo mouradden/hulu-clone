011a902aa5f092116e8496bf0c0704cd876d161c
DENGUIR Mourad <denguirmourad0@gmail.com>
2024-07-27 15:36:25 +0100

    styling and adding grid and flex

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "image.tmdb.org"],
-  },
-};
-module.exports = {
-  images: {
     remotePatterns: [
       {
         protocol: 'https',
